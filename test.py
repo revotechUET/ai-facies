c76f5e5cd047378bd6cc76e8a5435f943afefcdc
Hieu <tranhieu956230@gmail.com>
2019-05-17 16:38:04 +0700

    ok

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import unittest
 import pandas as pd
 import requests
 import json
-from csv import DictWriter
+from services.utilities import utils_func
+from copy import deepcopy
 
 
 class MyTest(unittest.TestCase):
@@ -24,24 +25,58 @@ class MyTest(unittest.TestCase):
 
         data = json.loads(res.text)
 
+        df = pd.DataFrame(data["content"], columns=["Boundary_flag"])
+        df.to_csv("./csv/debug_ub1.csv", index=False)
+
         print(data)
 
     def test_expert_rule(self):
-        data = pd.read_csv("csv/initial_data.csv")
+        initial_data = pd.read_csv("csv/initial_data.csv")
 
-        dct = {}
+        gr = initial_data.GR.values
+        tvd = initial_data.TVD.values
 
-        for key, value in data.items():
-            dct.update({key: list(value)})
+        data = {
+            "GR": list(gr),
+            "TVD": list(tvd)
+        }
 
         headers = {'content-type': 'application/json'}
+        url = 'http://127.0.0.1:9999/api/v1/unit-breakdown'
+
+        res1 = requests.post(url, data=json.dumps(data), headers=headers)
+
+        ub = json.loads(res1.text)
+
+        ub = ub["content"]
+
+        dct = {}
+        dct.update({"Boundary_flag": ub})
+        for key, value in initial_data.items():
+            dct.update({key: list(value)})
+
         url = 'http://0.0.0.0:9999/api/v1/expert-rule'
 
-        res = requests.post(url, data=json.dumps(dct), headers=headers)
+        res2 = requests.post(url, data=json.dumps(dct), headers=headers)
 
-        data = json.loads(res.text)
+        data1 = json.loads(res2.text)
 
-        print(data)
+        print(data1)
+
+        data1 = data1["content"]
+
+
+        tmp = []
+        for i in range(len(data1["Boundary_flag"])):
+            t = {}
+            for key in data1.keys():
+                t.update({key: data1[key][i]})
+
+            tmp.append(deepcopy(t))
+
+        utils_func.export_to_csv(tmp, "csv/T2X.csv")
+
+        # print(data)
 
         # if data["success"]:
         #     headers = list(data["payload"][0].keys())
